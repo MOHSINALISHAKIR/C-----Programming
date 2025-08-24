b68efc5dfcdc17dbbdd3293f700a5d646c24e51e
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-08-24 16:53:28 +0500

    datatypes and variables

---
+++ Day1/datatype.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // data
+    // the values given to a program to perform various operations on them in called data.
+    // the data type defines set of values and a set of operations that can be performed on those values
+    // data types in c++
+    // standrard data types -----> int float double char boolean short void
+    // user-defined data types 
+    int teaPrice = 23;
+    float waterTemp = 24.5;
+    double nationalAssembly = 120000;
+    char teaRanting = 'A';
+    bool isTeaFantistic = true;
+    cout<<"The Price of Tea: "<<teaPrice<<endl;
+    cout<<"Temp of water : "<<waterTemp<<endl;
+    cout<<"National Assembly Cost: "<<nationalAssembly<<endl;
+    cout<<"Tea Ranting: "<<teaRanting<<endl;
+    cout<<"Is Tea Fantistic or Not: "<<isTeaFantistic<<endl;
+
+    return 0;
+}
---
+++ Day1/variable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // varibale 
+    // a variable represents a named memory location . it is used to store the programs input data and its value during program execution
+     int num1 = 23;
+     int num2 = 34;
+     int sum;
+     sum = (num1 + num2);
+     cout<<"The Sum of num1 and num2 is = "<<sum<<endl;
+
+    return 0;
+
+}
