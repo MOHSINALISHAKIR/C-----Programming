76372cbbea5db1b3f6a28d5974f0bc04ccc1b46b
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-08-24 17:26:32 +0500

    strings

---
+++ Day1/string.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    //strings
+    // a string is a sequences of characters it may includes letters digits and various special characters such as + - * ete.
+    // declaring sting 
+    char name[] ="mohsin";
+    char city[] ="lahore";
+    char country[20];
+    cout<<"Enter Your Country"<<endl;
+    cin>>country;
+    cout<<"Your Country is    "<<country<<endl;
+
+
+    return 0;
+}
